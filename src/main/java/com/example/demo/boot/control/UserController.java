f20ceb8ac61958a329f6c364c81e488b5b89574a
tianminghai <tianminghai@aerozhonghuan.com>
2018-06-15 10:44:47 +0800

    1增加获取配置文件中变量方法 2配置文件修改servlet端口和Path前缀

--- src/main/java/com/example/demo/boot/control/UserController.java
+++ src/main/java/com/example/demo/boot/control/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.boot.control;
 
 import com.example.demo.boot.command.SimpleCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -11,18 +12,57 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/springboot")
 public class UserController {
-  @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
-  String getUserByGet(@RequestParam(value = "userName") String userName) {
-    System.out.println("---------getUserByGet start---------");
-    return "Hello " + userName;
-  }
 
-  @RequestMapping(value = "/getUserByPost", method = RequestMethod.POST)
-  String getUserByPost(@RequestParam(value = "userName") String userName, @RequestBody SimpleCommand data) {
-    System.out.println("---------getUserByPost start---------");
-    System.out.println("userName:"+userName +"  "+data.toString());
-    System.out.println("---------getUserByPost end---------");
-    return "Hello POST " + userName + data;
-  }
+    //    用户名
+    @Value("${username}")
+    private String userNameConf;
+    //    用户编码
+    @Value("${usercode}")
+    private String userCodeConf;
+
+    /**
+     * Get 方法
+     *
+     * @Param userName
+     * @Author:tianminghai
+     * @Date:上午10:37 2018/6/15
+     */
+    @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
+    String getUserByGet(@RequestParam(value = "userName") String userName) {
+
+        System.out.println("---------getUserByGet start---------");
+        return "Hello " + userName;
+    }
+
+    /**
+     * Post 方法
+     *
+     * @Param userName
+     * @Param data
+     * @Author:tianminghai
+     * @Date:上午10:37 2018/6/15
+     */
+    @RequestMapping(value = "/getUserByPost", method = RequestMethod.POST)
+    String getUserByPost(@RequestParam(value = "userName") String userName, @RequestBody SimpleCommand data) {
+
+        System.out.println("---------getUserByPost start---------");
+        System.out.println("userName:" + userName + "  " + data.toString());
+        System.out.println("---------getUserByPost end---------");
+        return "Hello POST " + userName + data;
+    }
+
+    /**
+     * 获取配置文件中的变量
+     *
+     * @Author:tianminghai
+     * @Date:上午10:42 2018/6/15
+     */
+    @RequestMapping(value = "/getConfigurationUserByGet", method = RequestMethod.GET)
+    String getConfigurationUserByGet() {
+        System.out.println("---------getConfigurationUserByGet start---------");
+        System.out.println("userNameConf:" + userNameConf + "  " + "userCodeConf：" + userCodeConf);
+        System.out.println("---------getConfigurationUserByGet end---------");
+        return "Hello Get " + userNameConf + " " + userCodeConf;
+    }
 
 }
